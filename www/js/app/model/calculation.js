c652c6a8f3fdd98f5f8533cba237b19aa060f205
mouylenglim <mouyleng@instedd.org>
2014-10-14 17:23:59 +0700

    calculation for yes no"

--- www/js/app/model/calculation.js
+++ www/js/app/model/calculation.js
@@ -5,22 +5,22 @@ Calculation = {
 
     var fields_cal = App.DataStore.get("fields_cal");
     fields_cal = JSON.parse(fields_cal);
-    
-    App.log("fields_cal : ", fields_cal);
 
     $.each(fields_cal, function(i, field_cal) {
       var code_cal = field_cal.config.code_calculation;
 
       $.each(field_cal.config.dependent_fields, function(j, dependent_field) {
         var $e = $("#" + elementPrefixId + this.id);
-        var val;
-        
-        if (dependent_field.kind == "select_one" || dependent_field.kind == "select_many"){ 
+        var val = $e.val();
+
+        if (dependent_field.kind == "select_one" || dependent_field.kind == "select_many")
           val = $("#" + elementPrefixId + this.id + " option:selected").text();
-          App.log("val : ", val);
+        else if (dependent_field.kind == "yes_no") {
+          if (val == 0)
+            val = false;
+          else
+            val = true;
         }
-        else
-          val = $e.val();
 
         if ($e.attr('class') == 'tree calculation') {
           val = $e.tree('getSelectedNode');
@@ -34,7 +34,6 @@ Calculation = {
 
         code_cal = code_cal.replace('$' + dependent_field.code, val);
       });
-      App.log("code_cal : ", code_cal);
       var cal_ele = $("#" + elementPrefixId + field_cal.idfield);
       var res = eval(code_cal);
       cal_ele.val(res);
