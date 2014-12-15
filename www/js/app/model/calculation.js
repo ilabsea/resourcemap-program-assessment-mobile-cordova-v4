f6149ed4f3e6edbccc677332981c4655513a290f
mouylenglim <mouyleng@instedd.org>
2014-12-15 15:16:36 +0700

    fix calculation on update

--- www/js/app/model/calculation.js
+++ www/js/app/model/calculation.js
@@ -4,35 +4,35 @@ Calculation = {
     var elementPrefixId = idElement.slice(0, idElement.lastIndexOf("_") + 1);
     var fields_cal = App.DataStore.get("fields_cal");
     fields_cal = JSON.parse(fields_cal);
+    
     $.map(fields_cal, function(field_cal) {
-      var cal_code = Calculation.generateSyntax(field_cal);
-      App.log("cal_code: ", cal_code);
+      var cal_code = Calculation.generateSyntax(field_cal, elementPrefixId);
 
       var cal_ele = $("#" + elementPrefixId + field_cal.idfield);
       cal_ele.val(eval(cal_code));
     });
   },
-  generateSyntax: function(field_cal) {
+  generateSyntax: function(field_cal, elementPrefixId) {
     var syntaxCal = field_cal.config.code_calculation;
     $.map(field_cal.config.dependent_fields, function(dependField) {
       var fieldName = "$" + dependField["code"];
-      var fieldValue = "$" + dependField["code"];
+      var fieldValue;
       switch (dependField["kind"]) {
         case "text":
         case "calculation":
         case "email":
         case "phone":
         case "date":
-          fieldValue = "$('#" + dependField["id"] + "').val()";
+          fieldValue = "$('#" + elementPrefixId +dependField["id"] + "').val()";
           break;
         case "numeric":
-          fieldValue = "parseInt($('#" + dependField["id"] + "').val())";
+          fieldValue = "parseInt($('#"+ elementPrefixId + dependField["id"] + "').val())";
           break;
         case "select_one":
-          fieldValue = "$('#" + dependField["id"] + " option:selected').text()";
+          fieldValue = "$('#" + elementPrefixId + dependField["id"] + " option:selected').text()";
           break;
         case "yes_no":
-          fieldValue = $("#" + dependField["id"]).val();
+          fieldValue = $("#" + elementPrefixId + dependField["id"]).val();
           if (fieldValue == 0)
             fieldValue = false;
           else
--- www/js/core/app.js
+++ www/js/core/app.js
@@ -1,4 +1,4 @@
-URL = "http://192.168.1.127:3000/";
+URL = "http://54.169.173.164/";
 END_POINT = URL + "api";
 App = {
   DB_SIZE: 5 * 1024 * 1024,
