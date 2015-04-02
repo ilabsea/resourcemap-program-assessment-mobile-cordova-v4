31caae31044d4aef601a6bce20a219813bc0d2d1
mouylenglim <mouyleng@instedd.org>
2015-04-02 11:05:48 +0700

    validate number field not allow to field in text

--- www/js/app/helper/number_field.js
+++ www/js/app/helper/number_field.js
@@ -2,11 +2,9 @@ var NumberField = {
   validateDecimal: function (element) {
     var config = JSON.parse(App.DataStore.get("configNumber_" + element.id));
     var val = $(element).val();
-    if (config.allows_decimals === "false") {
-      if (isNaN(val))
-        alert(element.name + " field has invalid number");
-      else if (val % 1 !== 0)
-        alert(element.name + " field must be integer");
-    }
+    if (isNaN(val))
+      alert(element.name + " field has invalid number");
+    else if (val % 1 !== 0 && config.allows_decimals === "false")
+      alert(element.name + " field must be integer");
   }
 };
