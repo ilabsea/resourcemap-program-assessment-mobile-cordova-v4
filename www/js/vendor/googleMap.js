eec5703607773760ec7e5455641c48d991364f07
mouylenglim <mouyleng@instedd.org>
2015-05-04 12:51:25 +0700

    push pin on map

--- www/js/vendor/googleMap.js
+++ www/js/vendor/googleMap.js
@@ -6,19 +6,18 @@ var mapObject = {
       this.loadMap();
     }
     else {
-      this.setMarker();
+      this.setMarker(this.getLatLng());
     }
   },
-  setMarker: function () {
-    var latlng = this.getLatLng();
+  setMarker: function (location) {
     _self = this;
     if (this.marker) {
-      this.marker.setPosition(latlng);
+      this.marker.setPosition(location);
     }
     else {
       this.marker = new google.maps.Marker({
         animation: google.maps.Animation.DROP,
-        position: latlng,
+        position: location,
         draggable: true,
         map: _self.map
       });
@@ -36,6 +35,8 @@ var mapObject = {
   loadMap: function () {
     var $content = $("#map_canvas");
     var mapCanvas = $content[0];
+    var longpress = false;
+    var start;
 
     var mapCanvasTop = $content.offset().top;
     $content.height(window.innerHeight - mapCanvasTop);
@@ -46,23 +47,47 @@ var mapObject = {
     };
     this.map = new google.maps.Map(mapCanvas, options);
 
-    this.setMarker();
+    this.setMarker(this.getLatLng());
     _self = this;
 
     google.maps.event.addListener(_self.marker, 'dragend', function () {
       var point = _self.marker.getPosition();
       var lat = point.lat();
       var lng = point.lng();
-      $("#updatelolat").val(lat);
-      $("#updatelolng").val(lng);
-      $("#updatelolat_online").val(lat);
-      $("#updatelolng_online").val(lng);
-      $("#lat").val(lat);
-      $("#lng").val(lng);
-      $("#mark_lat").val(lat);
-      $("#mark_lng").val(lng);
+      mapObject.setLatLngToElement(lat, lng);
       _self.map.panTo(point);
     });
+
+    google.maps.event.addListener(this.map, 'mousedown', function (event) {
+      $content.find('*').addClass('needsclick');
+      start = new Date().getTime();
+    });
+
+    google.maps.event.addListener(this.map, 'mouseup', function (event) {
+      var end = new Date().getTime();
+      longpress = (end - start < 1000) ? false : true;
+    });
+
+    google.maps.event.addListener(this.map, 'click', function (event) {
+      if (longpress) {
+        var point = event.latLng;
+        var lat = point.lat();
+        var lng = point.lng();
+
+        mapObject.setMarker(event.latLng);
+        mapObject.setLatLngToElement(lat, lng);
+      }
+    });
     google.maps.event.trigger(map_canvas, 'resize');
+  },
+  setLatLngToElement: function (lat, lng) {
+    $("#updatelolat").val(lat);
+    $("#updatelolng").val(lng);
+    $("#updatelolat_online").val(lat);
+    $("#updatelolng_online").val(lng);
+    $("#lat").val(lat);
+    $("#lng").val(lng);
+    $("#mark_lat").val(lat);
+    $("#mark_lng").val(lng);
   }
 };
